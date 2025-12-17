311a3c3af03e25255ee865bb09b370c852f186a8
Goutham Balaji P S <26gouthamba@gmail.com>
2025-12-17 21:55:32 +0530

    url added

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://password-reset-backend-xrn0.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
